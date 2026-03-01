aeacb842a61c74d3c1c33c771f7b4a2ef237c384
Hiya Sanghvi <169714000+hiyasanghvi@users.noreply.github.com>
2026-03-01 11:23:39 +0530

    Update opportunityApi.js

--- client/src/api/opportunityApi.js
+++ client/src/api/opportunityApi.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://placify-ooic.onrender.com/api",
 });
 
 // attach token automatically
